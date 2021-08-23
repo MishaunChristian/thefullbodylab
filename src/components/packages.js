d431aa4da52d02ad24242ae1623e1b2dfd8eff7b
Tatiana Mac <github@tatianamac.com>
2021-08-22 20:49:48 -0700

    Adds packages

--- src/components/packages.js
+++ src/components/packages.js
@@ -7,7 +7,18 @@ import ReactMarkdown from 'react-markdown';
 export default function Packages({ data }) {
   return (
     <>
-      <section className={classNames($.packages)}>
+      <section className={classNames($.packages, 'fblSection')}>
+        <h2 className={$.packageHeader}>Packages</h2>
+        <p className="textCenter">
+          All packages start with a 90-minute intake session. Individual sessions can be added on
+          after committing to a package.
+        </p>
+        <p className="textCenter">
+          Curious about prices?{' '}
+          <Link href="/">
+            <a className={$.teaserLink}>Send me a message</a>
+          </Link>
+        </p>
         <ol className={classNames($.packageContainer, 'fblSection flexAround alignItemsBetween')}>
           {data.map(session => (
             <li key={session._key} className={$.packageTile}>

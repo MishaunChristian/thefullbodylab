50d8df64abad90f14db1678f4f369214c4c5e5fd
Tatiana Mac <github@tatianamac.com>
2021-09-07 12:09:28 -0700

    Updates about-me

--- sanity/schemas/documents/about-me-page.js
+++ sanity/schemas/documents/about-me-page.js
@@ -17,6 +17,46 @@ export const aboutMePage = {
       title: 'Header',
       name: 'header',
       type: 'string'
+    },
+    {
+      title: 'My Approach Headline',
+      name: 'approachHead',
+      type: 'string'
+    },
+    {
+      title: 'My Approach Copy',
+      name: 'approach',
+      type: 'markdown'
+    },
+    {
+      title: 'Certification Headline',
+      name: 'certificationHead',
+      type: 'string'
+    },
+    {
+      title: 'Certification Copy',
+      name: 'certification',
+      type: 'markdown'
+    },
+    {
+      title: 'Goal Headline',
+      name: 'goalHead',
+      type: 'string'
+    },
+    {
+      title: 'Goal Copy',
+      name: 'goal',
+      type: 'markdown'
+    },
+    {
+      title: 'Background Headline',
+      name: 'backgroundHead',
+      type: 'string'
+    },
+    {
+      title: 'Background Copy',
+      name: 'background',
+      type: 'markdown'
     }
   ]
 };
--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -3,11 +3,20 @@ import Layout from '../components/layout';
 import Hero from '../components/hero';
 import { groq } from 'next-sanity';
 import { querySanity, usePreviewSubscription } from '../lib/sanity';
+import ReactMarkdown from 'react-markdown';
 
 const aboutMeQuery = groq`*[_type == "aboutMePage"][0]{
   title,
   header,
   eyebrow,
+  approachHead,
+  approach,
+  goalHead,
+  goal,
+  certificationHead,
+  certification,
+  backgroundHead,
+  background,
 }`;
 
 const metaQuery = groq`*[_type == "siteSettings"][0]{
@@ -19,10 +28,40 @@ export default function AboutMe({ metaData, initialData, preview }) {
   const { data } = usePreviewSubscription(aboutMeQuery, { initialData, enabled: preview });
   return (
     <Layout title={data.title} metaData={metaData}>
-      <div>
-        <Hero data={data} />
-      </div>
-      {/* https://codepen.io/liborgabrhel/pen/Ljzwxe */}
+      <Hero data={data} />
+      <section className="fblSection flexBetween">
+        <div className="copyBlock">
+          <h2 className="fblTextMedium">{data.approachHead}</h2>
+          <ReactMarkdown>{data.approach}</ReactMarkdown>
+          If this sounds, blah blah blah,{' '}
+          <a className="textLink" href="mailto:mishaun@thefullbodylab.com">
+            schedule a 20-min discovery session
+          </a>
+        </div>
+        <div className="copyBlock">
+          <h2 className="fblTextMedium">{data.certificationHead}</h2>
+          <ReactMarkdown>{data.certification}</ReactMarkdown>
+
+          <h2 className="fblTextMedium">{data.goalHead}</h2>
+          <ReactMarkdown>{data.goal}</ReactMarkdown>
+        </div>
+      </section>
+      <section>
+        <p className="fblTextQuote">
+          My interest in a holistic approach to healing started as I watched my dad struggle with
+          type II diabetes.
+        </p>
+      </section>
+      <section className="fblSection flexBetween">
+        <div className="copyBlock">
+          <h2 className="fblTextMedium">{data.backgroundHead}</h2>
+          <ReactMarkdown>{data.background}</ReactMarkdown>
+          {/* If this sounds, blah blah blah,{' '}
+          <a className="textLink" href="mailto:mishaun@thefullbodylab.com">
+            schedule a 20-min discovery session
+          </a> */}
+        </div>
+      </section>
     </Layout>
   );
 }
